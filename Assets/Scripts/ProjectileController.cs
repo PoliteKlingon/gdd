6d61d3bc892fc6944c6ad81fa670c1793591fbd6
Jiří Loun <514665@mail.muni.cz>
2022-11-28 17:47:39 +0100

    feat: cleanup in projectile controller

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -2,28 +2,10 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    private float _speed = 25.0f;
     private float _damage = 50.0f;
-    private Rigidbody _rigidbody;
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
-
-    public void SetSpeed(float speed)
-    {
-        _speed = speed;
-    }
-
+    
     public void SetDamage(float damage)
     {
         _damage = damage;
-    }
-
-    void Update()
-    {
-        /*if (_rigidbody.velocity.magnitude < 0.1)
-            _rigidbody.AddForce(transform.forward * _speed, ForceMode.VelocityChange);*/
     }
 }
