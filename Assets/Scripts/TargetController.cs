7c10dcf9b1f30892f37c1025ecb0fd4fef4ce9ee
Jiří Loun <514665@mail.muni.cz>
2022-12-06 15:41:26 +0100

    feat: progress on script for target

--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,24 @@ public class TargetController : MonoBehaviour
 {
     [SerializeField] private GameObject ship;
     
+    [SerializeField] private float defaultCastShift = 3;
+    [SerializeField] private float defaultShift = 50;
+
     //script for moving the target along the Z axis
     //cast a ray from ship forward
     //if it hits something, move the target to the point of hit
     //otherwise set it to default value
+    private void Update()
+    {
+        RaycastHit hit;
+        //TODO: remove unwanted layers fom the raycast target
+        if (Physics.Raycast(ship.transform.position + ship.transform.forward * defaultCastShift, ship.transform.forward, out hit, Mathf.Infinity))
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = ship.transform.position + ship.transform.forward * defaultShift;
+        }
+    }
 }
